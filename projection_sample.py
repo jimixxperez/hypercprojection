3a842986ca48c1d478370b98c7451f4c955e5436
jimixxperez <perezgemeinschaft@gmail.com>
2021-05-03 10:49:19 +0200

    refactored

--- projection_sample.py
+++ projection_sample.py
@@ -5,13 +5,6 @@ import numpy as np
 from scipy.spatial import distance
 from tqdm import tqdm
 
-G = nx.karate_club_graph()
-
-data = nx.adjacency_matrix(G).todense()
-
-N = data.shape[0]
-proj_plane = np.random.uniform(size=(N,2))
-
 def compute_energy(data, proj_plane):
     E = 0
     for i in range(N):
@@ -20,48 +13,60 @@ def compute_energy(data, proj_plane):
             ed = distance.euclidean(proj_plane[i,:], proj_plane[j,:])
             E += (ed-hd)**2
     return E
-  
-if __name__ == '__main__':  
-  step = 0.01
-  
-  temp = 1000
-  base = 0.95
-  T = lambda t: temp * base**(t)
 
-  curr_pos = proj_plane.copy()
-  curr_E = compute_energy(data, curr_pos)
+def run(data, epochs, step, T):
+    
+    N = data.shape[0]
+    
+    curr_pos = np.random.uniform(size=(N,2))
+    curr_E = compute_energy(data, curr_pos)
 
-
-  accepted_energy = np.zeros(epochs)
-  rejected_energy = np.zeros(epochs)
-  
-  epochs = 10000
-  for t in tqdm(range(epochs)):
-      rand_angle = np.random.uniform(
+    accepted_energy = np.zeros(epochs)
+    rejected_energy = np.zeros(epochs)
+    
+    for t in tqdm(range(epochs)):
+        rand_angle = np.random.uniform(
           low=0,high=2*np.pi,size=N
-      )
-      delta = step*np.array([
+        )
+        delta = step * np.array([
           np.cos(rand_angle),
           np.sin(rand_angle),
-      ]).reshape(-1,2)
+        ]).reshape(-1,2)
 
-      new_pos = curr_pos + delta
-      new_E = compute_energy(data, new_pos)
-      delta_E = (new_E - curr_E)
-      if delta_E > 0:
-          p = np.exp(-delta_E/T(t))
-          #print(delta_E, p)
-          draw = np.random.binomial(1, p)
-          if draw == 1:
-            #print('accept')
-            curr_pos = new_pos
+        new_pos = curr_pos + delta
+        new_E = compute_energy(data, new_pos)
+        delta_E = (new_E - curr_E)
+        if delta_E > 0:
+            p = np.exp(-delta_E/T(t))
+            #print(delta_E, p)
+            draw = np.random.binomial(1, p)
+            if draw == 1:
+                #print('accept')
+                curr_pos = new_pos
+                curr_E = new_E
+                accepted_energy[t] = new_E
+            else:
+                rejected_energy[t] = new_E
+                #print('decline')
+        else:
+            #print('accept 2')
             curr_E = new_E
             accepted_energy[t] = new_E
-        else:
-            rejected_energy[t] = new_E
-            #print('decline')
-    else:
-        #print('accept 2')
-        curr_E = new_E
-        accepted_energy[t] = new_E
-        curr_pos = new_pos
+            curr_pos = new_pos
+    return curr_pos, accepted_energy, rejected_energy
+  
+if __name__ == '__main__':
+    
+    temp = 1000
+    base = 0.95
+
+
+    epochs = 2000
+    step = 0.01
+    T = lambda t: temp * base**(t)
+    
+    
+    G = nx.karate_club_graph()
+    data = nx.adjacency_matrix(G).todense()
+
+    proj_plane, acc_E, rej_E = run(data, epochs, step, T)
